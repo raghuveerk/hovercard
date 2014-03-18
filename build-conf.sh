fd8d57e8913476d6abd0c2a2788fb409fea41159
jkamcc <juancamilocasas@gmail.com>
2014-03-18 16:01:10 -0600

    adding missing SDK folders and changing .gitignore rules

---
+++ build-conf.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+project_dir_name=hr-portal
+
+target_server=saffron.rivetlogic.com
+target_server_user=ci
+
+lr_bamboo_dir=/mnt/bamboo-5.0.1/builds/liferay-bamboo-build-deps
+
+valid_instances=DEV
